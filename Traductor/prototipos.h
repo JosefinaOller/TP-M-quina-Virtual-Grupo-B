79a35172c99dec881c843d6ed213b28e52f2715d
JosefinaOller <64923768+JosefinaOller@users.noreply.github.com>
2022-04-08 23:35:21 -0300

    Se empezó a programar el traductor.
    Se agregaron para introducir parámetros, la carga de los mnemonicos, el parser de Franco para la separación de los términos de la instrucción, la lectura del archivo ASM, la función para buscar la posición de rotulo.

---
+++ Traductor/prototipos.h
@@ -0,0 +1,15 @@
+#define MNEMAX 32 //la cantidad maxima de mnemonicos
+
+typedef struct{
+    char etiqueta[5];
+    int cantOp;
+    int codigo;
+} Mnemonico; //la estructura de mnemonico
+
+typedef struct{
+    char label[16];
+    char mnem[5];
+    char argA[16];
+    char argB[16];
+    char comentario[1025];
+}Linea; //la estructura de línea
