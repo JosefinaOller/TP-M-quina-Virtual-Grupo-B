9a9be007b278d4a3e3545c3ee6cfdcfb9e197847
AlanGutierrez39 <54786407+AlanGutierrez39@users.noreply.github.com>
2022-04-11 14:28:03 -0300

    Realicé la función de "codificaInstruccion"

--- Traductor/main.c
+++ Traductor/main.c
@@ -4,10 +4,17 @@
 #include <ctype.h>
 #include "prototipos.h"
 #include "parser.h"
-
-int main(int argc, const char *argv[])
-{
-
+void cargaVecMnem(Mnemonico[]);
+void traduce(char[], Mnemonico[], int, char[]);
+void estructuraASM(char[], Linea[], int*, char[][100], int*, int[]);
+void procesarLinea(char[], char[], char[], char[], char[], char[]);
+void lecturaLabels(char*, Label[], int*, int*);
+int busquedaLabel(Label[], char[], int, int*);
+int codificaInstruccion(Linea, Mnemonico[], Label[], int, int*);
+int tipoOperando(char[]);
+int anyToInt(char*, char**);
+int AEntero(char[]);
+int main(int argc, const char *argv[]){
     char asmar[20],binario[20];
     int o=1,i;
 
@@ -99,24 +106,17 @@ void traduce(char nombAsm[],Mnemonico vecMnem[],int o, char binario[]){ //Funcio
             fwrite(&header,sizeof(int),1,archESCRITURA);
 
             //codigo
-            /*
-            for(i=0;i<conLin;i++){
-                if(strcmp(codigo.mnem[i],"")!=0){
-                    inst=codificaInstruccion(&error); //a completar, hay que hacer la funcion de codificarInstruccion que arma la instruccion en un int
+
+            for(i=0;i<conLin;i++){//0-11
+                if(strcmp(codigo[i].mnem,"")!=0){
+                    inst=codificaInstruccion(codigo[i], vecMnem, rotulos, cantRotulos, &error); //a completar, hay que hacer la funcion de codificarInstruccion que arma la instruccion en un int
                     if(!error) //Si no hay error, grabo la instruccion codificada
                         fwrite(&inst,sizeof(int),1,archESCRITURA);
                 }
                 if(o){ //Si el flag de ocultamiento esta desactivado, muestro lineas
-
-
                 }
-
-            }*/
-
-
-
+            }
         }
-
         //Hay que poner lo de advertencia
     }
     fclose(archESCRITURA);
@@ -208,4 +208,122 @@ int busquedaLabel(Label rotulos[],char etiqueta[],int cantRotulos,int *error){ /
         *error=1;
         return -1;
     }
+}
+int codificaInstruccion(Linea codigo, Mnemonico vecMnem[], Label rotulos[], int cantRotulos, int *error){
+    int i, inst = 0;//Inicializo instrucción
+    Mnemonico mnem;
+    char *out = NULL;
+    for (i = 0; i < strlen(codigo.mnem); i++)
+        codigo.mnem[i] = toupper(codigo.mnem[i]);
+    i = 0;
+    while(i<24 && strcmp(codigo.mnem, vecMnem[i].etiqueta) != 0)
+        i++;
+    mnem = vecMnem[i];
+    if (mnem.cantOp == 2){
+        int topA = tipoOperando(codigo.argA);
+        int topB = tipoOperando(codigo.argB);
+        int vopA = (topA!=1) ? anyToInt(codigo.argA, &out) : AEntero(codigo.argA);
+        int vopB = (topB!=1) ? anyToInt(codigo.argB, &out) : AEntero(codigo.argB);
+        inst = (mnem.codigo << 28) | ((topA << 26) & 0x0C000000) | ((topB << 24) & 0x03000000) | ((vopA << 12) & 0x00FFF000) | (vopB & 0x00000FFF);
+    }else if (mnem.cantOp == 1){
+        int j = 0, top, vop;
+        while (j<cantRotulos && strcmp(codigo.argA, rotulos[j].etiqueta) != 0)
+            j++;
+        if (j<cantRotulos){
+            top = 0;//inmediato
+            vop = rotulos[j].codigo;
+        }
+        else{
+            top = tipoOperando(codigo.argA);
+            vop = (top!=1) ? anyToInt(codigo.argA, &out) : AEntero(codigo.argA);
+        }
+        inst = 0xF0000000 | ((mnem.codigo << 24) & 0x0F000000) | ((top << 22) & 0x03000000) | (vop & 0x0000FFFF);
+    }else
+        inst = 0xFF100000;
+    return inst;
+}
+int tipoOperando(char vop[]){
+    if (isdigit(vop[0]) || vop[0]=='#' || vop[0]=='@' || vop[0]=='%' || vop[0]=='$' || vop[0]=='\'')
+        return 0;//inmediato
+    else if (isalpha(vop[0]))
+        return 1;//de registro
+    else
+        return 2;//directo
+}
+int anyToInt(char *s, char **out){
+    char *BASES = {"**$*****@*#*****%"};
+    int base = 10;
+    char *bp = strchr(BASES, *s);
+    if (bp!=NULL){
+        base = bp - BASES;//Resta de punteros
+        ++s;
+    }
+    return strtol(s, out, base);
+}
+int AEntero(char vop[]){
+    //Registros = {"DS","","","","","IP","","","CC","AC","A","B","C","D","E","F"};
+    int valor;
+    if (strcmp(vop, "DS")==0)
+        valor = 0x0;
+    else if (strcmp(vop, "IP")==0)
+        valor = 0x5;
+    else if (strcmp(vop, "CC")==0)
+        valor = 0x8;
+    else if (strcmp(vop, "EAX")==0 || strcmp(vop, "AX")==0 || strcmp(vop, "AH")==0 || strcmp(vop, "AL")==0){
+        valor = 0xA;
+        if (strcmp(vop, "AX")==0)
+            valor = valor | (0x3 << 4);
+        else if (strcmp(vop, "AH")==0)
+            valor = valor | (0x2 << 4);
+        else
+            valor = valor | (0x1 << 4);
+    }
+    else if (strcmp(vop, "EBX")==0 || strcmp(vop, "BX")==0 || strcmp(vop, "BH")==0 || strcmp(vop, "BL")==0){
+        valor = 0xB;
+        if (strcmp(vop, "BX")==0)
+            valor = valor | (0x3 << 4);
+        else if (strcmp(vop, "BH")==0)
+            valor = valor | (0x2 << 4);
+        else
+            valor = valor | (0x1 << 4);
+    }
+    else if (strcmp(vop, "ECX")==0 || strcmp(vop, "CX")==0 || strcmp(vop, "CH")==0 || strcmp(vop, "CL")==0){
+        valor = 0xC;
+        if (strcmp(vop, "CX")==0)
+            valor = valor | (0x3 << 4);
+        else if (strcmp(vop, "CH")==0)
+            valor = valor | (0x2 << 4);
+        else
+            valor = valor | (0x1 << 4);
+    }
+    else if (strcmp(vop, "EDX")==0 || strcmp(vop, "DX")==0 || strcmp(vop, "DH")==0 || strcmp(vop, "DL")==0){
+        valor = 0xD;
+        if (strcmp(vop, "DX")==0)
+            valor = valor | (0x3 << 4);
+        else if (strcmp(vop, "DH")==0)
+            valor = valor | (0x2 << 4);
+        else
+            valor = valor | (0x1 << 4);
+    }
+    else if (strcmp(vop, "EEX")==0 || strcmp(vop, "EX")==0 || strcmp(vop, "EH")==0 || strcmp(vop, "EL")==0){
+        valor = 0xE;
+        if (strcmp(vop, "EX")==0)
+            valor = valor | (0x3 << 4);
+        else if (strcmp(vop, "EH")==0)
+            valor = valor | (0x2 << 4);
+        else
+            valor = valor | (0x1 << 4);
+    }
+    else if (strcmp(vop, "EFX")==0 || strcmp(vop, "FX")==0 || strcmp(vop, "FH")==0 || strcmp(vop, "FL")==0){
+        valor = 0xF;
+        if (strcmp(vop, "FX")==0)
+            valor = valor | (0x3 << 4);
+        else if (strcmp(vop, "FH")==0)
+            valor = valor | (0x2 << 4);
+        else
+            valor = valor | (0x1 << 4);
+    }
+    else//AC
+        valor = 0x9;
+    return valor;
 }
